eef7168811805dcef2f2dd197b787076e3908b6a
AlekOmOm <142972647+AlekOmOm@users.noreply.github.com>
2024-11-10 15:42:39 +0100

    init

---
+++ src/main/java/com/alek0m0m/aicookbookbackend/AiCookBookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alek0m0m.aicookbookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCookBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCookBookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alek0m0m/aicookbookbackend/AiCookBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alek0m0m.aicookbookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCookBookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
